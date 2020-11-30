6b3b2e1eccd7dd416688ee348080f3f4a5e92ad7
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-11-30 07:41:07 +0530

    1D array max consecutive sum

---
+++ 450Basic/src/Array/MaxSumArray.java
@@ -0,0 +1,17 @@
+package Array;
+
+import java.io.*;
+import java.util.*;
+
+public class MaxSumArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int arr[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int sum = 0, best = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = Math.max(arr[i], sum + arr[i]);
+            best = Math.max(sum, best);
+        }
+        System.out.println(best);
+    }
+}
