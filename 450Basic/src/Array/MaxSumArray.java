5c51c93fd40d4edc79b2993befa2ea0abcc3f5a5
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-12-07 01:07:02 +0530

    correction in max sum array

--- 450Basic/src/Array/MaxSumArray.java
+++ 450Basic/src/Array/MaxSumArray.java
@@ -7,7 +7,7 @@ public class MaxSumArray {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int arr[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int sum = 0, best = 0;
+        int sum = 0, best = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             sum = Math.max(arr[i], sum + arr[i]);
             best = Math.max(sum, best);
